3740bfeae155af35bcacf6f718775f3122c0fe96
Jerome Flesch <jflesch@gmail.com>
2016-08-29 16:43:57 +0200

    Drop the Python implementation of ACE
    Signed-off-by: Jerome Flesch <jflesch@gmail.com>

--- run_tests.py
+++ run_tests.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 import unittest
 
-from tests import tests_util
 from tests import tests_ace
 
 if __name__ == '__main__':
-    # unittest.main(tests_util, verbosity=2, exit=False)
     unittest.main(tests_ace, verbosity=2, exit=False)

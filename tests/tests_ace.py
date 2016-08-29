a72f4608f63de1f7363a847f92bc00bdd5decf59
Jerome Flesch <jflesch@gmail.com>
2016-08-29 16:50:16 +0200

    Turn the ACE test into a real test
    Signed-off-by: Jerome Flesch <jflesch@gmail.com>

--- tests/tests_ace.py
+++ tests/tests_ace.py
@@ -1,3 +1,4 @@
+import tempfile
 import unittest
 
 import PIL.Image
@@ -7,6 +8,17 @@ from pypillowfight import ace
 
 class TestACE(unittest.TestCase):
     def test_ace(self):
-        img = PIL.Image.open("tests/data/brightness_problem.jpg")
-        img = ace.ace(img, seed=12345)
-        img.save("tests/data/brightness_problem_ace.jpg")
+        with tempfile.NamedTemporaryFile(suffix='.jpg') as tmpfile:
+            in_img = PIL.Image.open("tests/data/brightness_problem.jpg")
+            expected_img = PIL.Image.open("tests/data/brightness_problem_ace.jpg")
+
+            out_img = ace.ace(in_img, seed=12345)
+            in_img.close()
+
+            # beware of JPG compression
+            out_img.save(tmpfile.name)
+            out_img.close()
+            out_img = PIL.Image.open(tmpfile.name)
+
+        self.assertEqual(out_img.tobytes(), expected_img.tobytes())
+        expected_img.close()

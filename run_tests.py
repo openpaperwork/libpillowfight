150f0f846aee8e1fbe102ef75a5bde4d0270f51c
Jerome Flesch <jflesch@gmail.com>
2016-08-30 14:32:05 +0200

    Re-enable all tests
    Signed-off-by: Jerome Flesch <jflesch@gmail.com>

--- run_tests.py
+++ run_tests.py
@@ -7,7 +7,7 @@ from tests import tests_blurfilter
 from tests import tests_noisefilter
 
 if __name__ == '__main__':
-    #unittest.main(tests_ace, verbosity=2, exit=False)
-    #unittest.main(tests_blackfilter, verbosity=2, exit=False)
+    unittest.main(tests_ace, verbosity=2, exit=False)
+    unittest.main(tests_blackfilter, verbosity=2, exit=False)
     unittest.main(tests_blurfilter, verbosity=2, exit=False)
-    #unittest.main(tests_noisefilter, verbosity=2, exit=False)
+    unittest.main(tests_noisefilter, verbosity=2, exit=False)

d013b1d06c1d29c6a854f340b50043ba204b9bd7
Jerome Flesch <jflesch@gmail.com>
2016-08-30 19:17:10 +0200

    Fix copyright headers in source files
    Signed-off-by: Jerome Flesch <jflesch@gmail.com>

--- src/pypillowfight/util.c
+++ src/pypillowfight/util.c
@@ -1,11 +1,11 @@
 /*
  * Copyright © 2016 Jerome Flesch
  *
- * Unpaper is free software; you can redistribute it and/or modify
+ * Pypillowfight is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, version 2 of the License.
  *
- * Unpaper is distributed in the hope that it will be useful,
+ * Pypillowfight is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.

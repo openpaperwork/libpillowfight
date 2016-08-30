f309f985238ac98120bfff5525451b90368e1473
Jerome Flesch <jflesch@gmail.com>
2016-08-31 01:40:21 +0200

    Add another test using all the algorithms of unpaper + ACE
    Signed-off-by: Jerome Flesch <jflesch@gmail.com>

--- tests/tests_all.py
+++ tests/tests_all.py
@@ -13,7 +13,7 @@ from pypillowfight import noisefilter
 
 
 class TestAll(unittest.TestCase):
-    def test_all(self):
+    def test_all_1(self):
         with tempfile.NamedTemporaryFile(suffix='.jpg') as tmpfile:
             in_img = PIL.Image.open("tests/data/black_border_problem.jpg")
 
@@ -36,6 +36,33 @@ class TestAll(unittest.TestCase):
 
         expected_img = PIL.Image.open(
             "tests/data/black_border_problem_all.jpg"
+        )
+        self.assertEqual(out_img.tobytes(), expected_img.tobytes())
+        expected_img.close()
+
+    def test_all_2(self):
+        with tempfile.NamedTemporaryFile(suffix='.jpg') as tmpfile:
+            in_img = PIL.Image.open("tests/data/brightness_problem.jpg")
+
+            out_img = ace.ace(in_img, seed=0xBEEDEAD)
+
+            # unpaper order
+            out_img = blackfilter.blackfilter(out_img)
+            out_img = noisefilter.noisefilter(out_img)
+            out_img = blurfilter.blurfilter(out_img)
+            out_img = masks.masks(out_img)
+            out_img = grayfilter.grayfilter(out_img)
+            out_img = border.border(out_img)
+
+            in_img.close()
+
+            # beware of JPG compression
+            out_img.save(tmpfile.name)
+            out_img.close()
+            out_img = PIL.Image.open(tmpfile.name)
+
+        expected_img = PIL.Image.open(
+            "tests/data/brightness_problem_all.jpg"
         )
         self.assertEqual(out_img.tobytes(), expected_img.tobytes())
         expected_img.close()

a350f3c63e9d5e1bca02bd682e3a84a770f50812
Jerome Flesch <jflesch@gmail.com>
2016-09-02 01:11:12 +0200

    Implements the Stroke Width detection in SWT (part of the algorithm still missing)
    Signed-off-by: Jerome Flesch <jflesch@gmail.com>

--- src/pypillowfight/_swt.c
+++ src/pypillowfight/_swt.c
@@ -40,7 +40,12 @@
 
 #define DARK_ON_LIGHT 1
 
-#define DEFAULT_NB_POINTS_PER_RAY 16
+#define DEF_NB_POINTS_PER_RAY 32
+
+#define PRECISION 0.05
+
+#define IS_EDGE_LINE(val) ((val) < (PF_WHITE - 1.0))
+
 
 struct swt_point {
 	int x;
@@ -48,13 +53,10 @@ struct swt_point {
 };
 
 struct swt_ray {
-	struct swt_point *points;
-	int nb_points;
-
-	struct swt_point *beginning;
-	struct swt_point *end;
-
 	struct swt_ray *next;
+
+	int nb_points;
+	struct swt_point points[];
 };
 
 struct swt_output {
@@ -62,9 +64,170 @@ struct swt_output {
 	struct swt_ray *rays;
 };
 
+static void init_output(struct swt_output *out)
+{
+	int x, y;
+
+	for (x = 0 ; x < out->swt.size.x ; x++) {
+		for (y = 0 ; y < out->swt.size.y ; y++) {
+			PF_MATRIX_SET(&out->swt, x, y, DBL_MAX);
+		}
+	}
+}
+
+static void add_point_to_ray(const struct swt_point *current, void *callback_data) {
+	struct swt_ray *ray = callback_data;
+	ray->points[ray->nb_points] = *current;
+	ray->nb_points++;
+}
+
+/*!
+ * \brief follow the supposed stroke to its end and make sure the angles match
+ *
+ * \retval 0 if no match (--> no stroke)
+ * \return number of points in the ray
+ */
+static int follow_stroke(
+		const struct pf_dbl_matrix *edge,
+		const struct pf_gradient_matrixes *gradient,
+		int x, int y,
+		void (*on_ray_point_callback)(const struct swt_point *current, void *callback_data),
+		void *callback_data)
+{
+	double g_x, g_y, g_x_end, g_y_end;
+	double cur_x, cur_y;
+	struct swt_point current_pt;
+	int nb_points = 0;
+
+	g_x = cos(PF_MATRIX_GET(&gradient->direction, x, y));
+	g_y = sin(PF_MATRIX_GET(&gradient->direction, x, y));
+	if (DARK_ON_LIGHT) {
+		g_x = -g_x;
+		g_y = -g_y;
+	}
+
+	current_pt.x = x;
+	current_pt.y = y;
+	cur_x = 0.5 + x;
+	cur_y = 0.5 + y;
+
+	while(1) {
+		// Move along the ray by incrementing with reduced values of (g_x, g_y)
+		// TODO(Jflesch): Optim ?
+		cur_x += g_x * PRECISION;
+		cur_y += g_y * PRECISION;
+
+		if ((floor(cur_x) == current_pt.x)
+				&& (floor(cur_y) == current_pt.y)) {
+			continue;
+		}
+		current_pt.x = floor(cur_x);
+		current_pt.y = floor(cur_y);
+
+		if (current_pt.x < 0
+				|| current_pt.x >= gradient->direction.size.x
+				|| current_pt.y < 0
+				|| current_pt.y >= gradient->direction.size.y)
+			break;
+
+		nb_points++;
+		if (on_ray_point_callback)
+			on_ray_point_callback(&current_pt, callback_data);
+
+		if (IS_EDGE_LINE(PF_MATRIX_GET(edge, current_pt.x, current_pt.y))) {
+			break;
+		}
+	}
+
+	assert(current_pt.x != x || current_pt.y != y);
+
+	g_x_end = cos(PF_MATRIX_GET(
+			&gradient->direction, current_pt.x, current_pt.y
+		));
+	g_y_end = sin(PF_MATRIX_GET(
+			&gradient->direction, current_pt.x, current_pt.y
+		));
+	if (DARK_ON_LIGHT) {
+		g_x_end = -g_x_end;
+		g_y_end = -g_y_end;
+	}
+
+	if (acos((g_x * -g_x_end) + (g_y * -g_y_end) >= (M_PI / 2.0))) {
+		// no stroke here
+		return 0;
+	}
+	return 1;
+}
+
+static void find_stroke(struct swt_output *out,
+		const struct pf_dbl_matrix *edge,
+		const struct pf_gradient_matrixes *gradient,
+		int x, int y)
+{
+	int nb_points, i;
+	struct swt_ray *ray;
+	double val, length;
+
+	nb_points = follow_stroke(edge, gradient, x, y, NULL, NULL);
+	if (nb_points <= 0) {
+		// no stroke here
+		return;
+	}
+
+	// we found one --> fill in a ray structure
+	ray = malloc(sizeof(struct swt_ray) + (sizeof(struct swt_point) * nb_points));
+	ray->next = NULL;
+	ray->nb_points = 0;
+	nb_points = follow_stroke(edge, gradient, x, y, add_point_to_ray, ray);
+	assert(nb_points > 0);
+	assert(nb_points == ray->nb_points);
+
+	// length of the stroke
+	length = hypot(
+		ray->points[0].x - ray->points[ray->nb_points - 1].x,
+		ray->points[0].y - ray->points[ray->nb_points - 1].y
+	);
+
+	// add stroke's ray to the output
+	ray->next = out->rays;
+	out->rays = ray;
+
+	// follow the ray: each point that has a current score bigger
+	// than the length must have its score set to the length
+	for (i = 0 ; i < nb_points ; i++) {
+		val = PF_MATRIX_GET(&out->swt, ray->points[i].x, ray->points[i].y);
+		val = MIN(val, length);
+		PF_MATRIX_SET(&out->swt, ray->points[i].x, ray->points[i].y, val);
+	}
+}
+
 struct swt_output swt(const struct pf_dbl_matrix *edge, const struct pf_gradient_matrixes *gradient)
 {
+	int x, y;
 	struct swt_output out;
+
+	assert(edge->size.x == gradient->intensity.size.x);
+	assert(edge->size.y == gradient->intensity.size.y);
+	assert(edge->size.x == gradient->direction.size.x);
+	assert(edge->size.y == gradient->direction.size.y);
+
+	memset(&out, 0, sizeof(out));
+	out.swt = pf_dbl_matrix_new(edge->size.x, edge->size.y);
+	init_output(&out);
+
+	for (x = 0 ; x < edge->size.x ; x++) {
+		for (y = 0 ; y < edge->size.y ; y++) {
+
+			if (IS_EDGE_LINE(PF_MATRIX_GET(edge, x, y))) {
+				continue;
+			}
+
+			find_stroke(&out, edge, gradient, x, y);
+
+		}
+	}
+
+	// TODO
 
 	return out;
 }

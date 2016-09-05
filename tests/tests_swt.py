1dad7e94b82874ba445275b72eb42ce4edce8a91
Jerome Flesch <jflesch@gmail.com>
2016-09-05 08:46:27 +0200

    SWT: Add test
    Signed-off-by: Jerome Flesch <jflesch@gmail.com>

--- tests/tests_swt.py
+++ tests/tests_swt.py
@@ -23,3 +23,20 @@ class TestSWT(unittest.TestCase):
         )
         self.assertEqual(out_img.tobytes(), expected_img.tobytes())
         expected_img.close()
+
+    def test_swt2(self):
+        with tempfile.NamedTemporaryFile(suffix='.jpg') as tmpfile:
+            in_img = PIL.Image.open("tests/data/black_border_problem.jpg")
+            out_img = pypillowfight.swt(in_img)
+            in_img.close()
+
+            # beware of JPG compression
+            out_img.save(tmpfile.name)
+            out_img.close()
+            out_img = PIL.Image.open(tmpfile.name)
+
+        expected_img = PIL.Image.open(
+            "tests/data/black_border_problem_swt.jpg"
+        )
+        self.assertEqual(out_img.tobytes(), expected_img.tobytes())
+        expected_img.close()
